3dd480211e99031dd20a1948ee1b64d32344dd84
slavcopost <slavcopost@gmail.com>
2013-04-18 14:17:30 +0300

    Fix troubles with UTF-8

--- SlugBehavior.php
+++ SlugBehavior.php
@@ -61,6 +61,9 @@ class SlugBehavior extends CBehavior
 
 	protected function generateSlug($string)
 	{
+		// Make sure string is in UTF-8 and strip invalid UTF-8 characters
+		$string = mb_convert_encoding((string) $string, 'UTF-8', mb_list_encodings());
+
 		// Make custom replacements
 		$string = preg_replace(array_keys($this->replacements), $this->replacements, $string);
 
@@ -75,12 +78,12 @@ class SlugBehavior extends CBehavior
 		$string = preg_replace('/(' . preg_quote($this->delimiter, '/') . '){2,}/', '$1', $string);
 
 		if ((int) $this->length > 0) {
-			$string = substr($string, 0, $this->length);
+			$string = mb_substr($string, 0, $this->length, 'UTF-8');
 		}
 
 		// Remove delimiter from ends
 		$string = trim($string, $this->delimiter);
 
-		return $this->lowercase ? strtolower($string) : $string;
+		return $this->lowercase ? mb_strtolower($string, 'UTF-8') : $string;
 	}
 }
